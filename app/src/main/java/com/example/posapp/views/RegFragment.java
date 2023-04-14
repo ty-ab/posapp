93f8e3443408edad68539bbca06ce4b3290278c5
ty-ab <tayyebirhan@gmail.com>
2023-04-14 13:23:46 +0300

    update register fragment

--- app/src/main/java/com/example/posapp/views/RegFragment.java
+++ app/src/main/java/com/example/posapp/views/RegFragment.java
@@ -8,11 +8,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import androidx.fragment.app.Fragment;
 import androidx.room.Room;
 
 import com.example.posapp.databinding.FragmentRegBinding;
+import com.example.posapp.db.AppDatabase;
+import com.example.posapp.db.RegisterItem;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Completable;
@@ -43,17 +50,33 @@ public class RegFragment extends Fragment {
         btnResetReg = regBinding.resetregid;
         btnSaveReg = regBinding.saveregid;
 
+        AppDatabase _db = Room.databaseBuilder(this.getContext(),
+                AppDatabase.class, "registered").build();
 
         //RegItemDao regItemDao = _db.regItemDao();
 
         btnSaveReg.setOnClickListener(v -> {
+
+                String code = codeRegTextEdit.getText().toString();
+                String _item = itemItemTextEdit.getText().toString();
+                String unit = unitRegTextEdit.getText().toString();
+                float unitPrice = Float.parseFloat(unitPriceRegTextEdit.getText().toString());
+
+                Completable completable = _db.regItemDao().insert(new RegisterItem(code,_item, unit,unitPrice));
+                completable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread()).subscribe(() -> {
+                            Toast.makeText(this.getContext(), "item saved", Toast.LENGTH_SHORT).show();
+                        }, throwable -> {
+                            Toast.makeText(this.getContext(), "failed", Toast.LENGTH_SHORT).show();
+                        });
+                //RegisterItem registerItem = new RegisterItem();
+                //registerItem.insertAll();
 
 
         });
 
         btnResetReg.setOnClickListener(v -> {
             Toast.makeText(this.getContext(), "Clear Button Clicked", Toast.LENGTH_SHORT).show();
-
         });
 
 
